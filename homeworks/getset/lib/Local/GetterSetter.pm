a014222f45290bc3c22904c4d8da3c53c71d2ea7
Luna-alina95 <AlinaBuriy@gmail.com>
2016-03-18 18:03:05 +0300

    getset

--- homeworks/getset/lib/Local/GetterSetter.pm
+++ homeworks/getset/lib/Local/GetterSetter.pm
@@ -1,49 +1,22 @@
-package Local::GetterSetter;
+{
+package lib::newmodul;
 
 use strict;
 use warnings;
-
-use Exporter 'import';
-our @EXPORT = qw(get_x set_x);
-
-=encoding utf8
-
-=head1 NAME
-
-Local::GetterSetter - getters/setters generator
-
-=head1 VERSION
-
-Version 1.00
-
-=cut
-
-our $VERSION = '1.00';
-
-=head1 SYNOPSIS
-
-    package Local::SomePackage;
-    use Local::GetterSetter qw(x y);
-
-    set_x(50);
-    print our $x; # 50
-
-    our $y = 42;
-    print get_y(); # 42
-    set_y(11);
-    print get_y(); # 11
-
-=cut
-my $x;
-
-sub set_x {
-	$x = shift();
-	return $x;
+use Data::Dumper; 
+sub import {
+	my ($class, @vars) = @_;
+	my ($package, $filename, $line) = caller();
+	foreach my $k (@vars) {
+		no strict 'refs';
+		*{$package.'::'."set_$k"} = sub {
+			${$package.'::'.$k} = shift;
+		};
+		
+		*{$package.'::'."get_$k"} = sub {
+		  	return ${$package.'::'.$k};
+		};
 	}
-
-sub get_x {
-	return $x;
-	}
-
-
+}
 1;
+}
